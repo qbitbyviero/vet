f93b8dfe759f95c89edaae3cbbd06b118bf7d0d0
qbitbyviero <qbit.by.fjmg@gmail.com>
2025-06-04 17:01:10 -0600

    main.js v3.7

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // ======================================
   // 0) URL DE TU WEB APP (Apps Script)
   // ======================================
-  const GAS_BASE_URL = "https://script.google.com/macros/s/AKfycbz-BDi2dE8_mh-Wg5Vnlos54zFD28pJ-QVU9ApPhT3hS1ugRQXcI-75dQmghflg_EgpJA/exec";
+  const GAS_BASE_URL = "https://script.google.com/macros/s/AKfycbz5DFuZiy27bHUANMDyDcPSpZzGmMNovTQUzzfFAi9qnnNvVaP-HwJivg2YgXAlr2WyQQ/exec";
 
   // ============================
   // 1) JSONP REQUEST (para evitar CORS)
